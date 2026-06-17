fe743f967b98fa8beaea2d5c555a31a9f7455f01
Nitesh Pandey <niteshpandey8969@gmail.com>
2026-06-18 00:24:57 +0530

    get me

--- frontend/src/features/auth/auth.context.jsx
+++ frontend/src/features/auth/auth.context.jsx
@@ -25,7 +25,7 @@ export const AuthProvider = ({ children }) => {
     // }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser, loading , getMe, setLoading}}>
+    <AuthContext.Provider value={{ user, setUser, loading ,  setLoading}}>
       {children}
     </AuthContext.Provider>
   );
